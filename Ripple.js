e1379ebc482ea8f77788299ebcd3cbb86e757986
Nicholas Walkewicz <nwalkewicz@gmail.com>
2022-06-10 10:46:37 -0700

    Initial Commit

---
+++ Ripple.js
@@ -0,0 +1,83 @@
+const DEFAULT_OPTIONS = {
+	size: 100,
+	color: 'hsl(0 0% 0% / 0.1)',
+	position: {
+		x: 0,
+		y: 0
+	},
+	timeout: 500
+}
+
+class Ripple {
+	#rippleElem;
+	#activeSince;
+	#removeBinded;
+	#readyToRemove;
+	
+	/**
+	 * @param {Object} options
+	 * @param {HTMLElement} options.target
+	 */
+	constructor(options) {
+		this.#rippleElem = document.createElement('div');
+		this.#rippleElem.classList.add('rippleElem');
+		this.#activeSince = new Date();
+		this.#removeBinded = this.#remove.bind(this);
+		this.#initializeStyles();
+		requestAnimationFrame(() => this.#rippleElem.classList.add('show'));
+		if (!options.color) delete options.color;
+		this.#update({...DEFAULT_OPTIONS, ...options});
+		setTimeout(this.#removeBinded, this.timeout);
+	}
+
+	set target(target) {
+		target.append(this.#rippleElem);
+	}
+
+	set color(value) {
+		this.#rippleElem.style.backgroundColor = value;
+	}
+
+	set size(value) {
+		this.#rippleElem.style.width = value;
+		this.#rippleElem.style.height = value;
+	}
+
+	set position({x, y}) {
+		this.#rippleElem.style.left = x;
+		this.#rippleElem.style.top = y;
+	}
+
+	set readyToRemove(value) {
+		this.#readyToRemove = value;
+		if (!value) return;
+		if (new Date() - this.#activeSince >= this.timeout) this.#removeBinded();
+	}
+
+	get readyToRemove() {
+		return this.#readyToRemove;
+	}
+
+	#initializeStyles() {
+	}
+
+	#update(options) {
+		options.position.x = options.position.x - (options.size / 2);
+		options.position.y = options.position.y - (options.size / 2);
+		options.position.x += 'px';
+		options.position.y += 'px';
+		options.size += 'px';
+		Object.entries(options).forEach(([key, val]) => this[key] = val);
+	}
+
+	#remove() {
+		if (!this.readyToRemove) return;
+		this.#rippleElem.classList.add('fadeOut');
+		this.#rippleElem.addEventListener('transitionend', e => {
+			if (e.propertyName !== 'opacity') return;
+			this.#rippleElem.remove();
+		});
+	}
+}
+
+export default Ripple;
---
+++ mat-ripple.js
@@ -0,0 +1,42 @@
+import Ripple from '/Ripple.js';
+
+const rTargets = document.querySelectorAll('[ripple]');
+	
+function readyToRemove(e, ripple) {
+	if (e.target.getAttribute('ripple') === null) return false;
+	return ripple.readyToRemove = true;
+}
+
+rTargets.forEach(target => {
+	let ripple;
+	
+	target.addEventListener('pointerdown', e => {
+		// Filter out child elements
+		if (e.target.getAttribute('ripple') === null) return;
+
+		// Set position
+		const rData = {
+			position: {
+				x: e.layerX,
+				y: e.layerY
+			}
+		}
+
+		// Calculate pixel offset from center of element
+		const offsetX = Math.abs((e.target.offsetWidth / 2) - rData.position.x);
+		const offsetY = Math.abs((e.target.offsetHeight / 2) - rData.position.y);
+		const offsetH = Math.hypot(offsetX, offsetY);
+
+		// Set size based on element hypotenuse & pixel offset
+		rData.size = Math.hypot(e.target.offsetWidth, e.target.offsetHeight) + (offsetH * 2);
+
+		// Set color
+		rData.color = e.target.getAttribute('ripple-color') || undefined;
+
+		// Create ripple
+		ripple = new Ripple({target, ...rData});
+	});
+
+	target.addEventListener('pointerup', e => readyToRemove(e, ripple));
+	target.addEventListener('pointerleave', e => readyToRemove(e, ripple));
+});
